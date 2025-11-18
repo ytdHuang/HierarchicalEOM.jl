db4c83e2dcc9cf71e9f22bfa882cf07471729e9c
Yi-Te Huang <44385685+ytdHuang@users.noreply.github.com>
2025-11-18 14:11:29 +0800

    fix typo

--- src/evolution.jl
+++ src/evolution.jl
@@ -345,7 +345,7 @@ function _generate_Eops(M::AbstractHEOMLSMatrix, e_ops)
     ]
     return tr_e_ops
 end
-_generate_Eops(M::AbstractHEOMLSMatrix, e_ops::AbstractVector{T}, Id_sys, Id_HEOM) where {T<:ADOs} =
+_generate_Eops(M::AbstractHEOMLSMatrix, e_ops::AbstractVector{T}) where {T<:ADOs} =
     [_HandleTraceVectorType(M, ados.data) for ados in e_ops]
 
 struct SaveFuncHEOMSolve{TE,PT<:Union{Nothing,Progress},IT,TEXPV<:Union{Nothing,AbstractMatrix}} <: AbstractSaveFunc
